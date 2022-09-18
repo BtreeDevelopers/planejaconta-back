f388fcc795fe3d957c9f61e1f6eaafc1cf1152bb
GanekoGuilherme <guilhermeganeko@hotmail.com>
2022-09-17 22:57:43 -0300

    feat(RouteRoot): inserindo rota raiz para indicar que a palicação está rodando.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,10 @@ const routes = Router();
 
 routes.use("/user", userRoutes);
 
+routes.get("/", (_request, response) =>
+  response.status(200).json({ message: "Aplication is running!" })
+);
+
 routes.get("/health-check", (_request, response) =>
   response.status(200).json({ uptime: Math.floor(process.uptime()) })
 );
