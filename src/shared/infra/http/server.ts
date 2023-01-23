19a07689fc77cc7c8bacc4a81d4f4ccd359f14e9
thalysm <42477970+thalysm@users.noreply.github.com>
2023-01-23 16:40:33 -0300

    teste

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,6 +4,6 @@ import { Express } from "express";
 boot.then(() => {
   const app = require("./app") as Express;
   app.listen(process.env.PORT || 3000, () =>
-    console.log(`Server is running on ${process.env.PORT || 3000}`)
+    console.log(`Server is running on ${process.env.PORT || 3000}.`)
   );
 });
